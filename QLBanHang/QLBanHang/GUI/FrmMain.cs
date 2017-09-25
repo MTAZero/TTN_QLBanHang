edeecdd40d920c76ceac66a6f7c5cb6458bbdb78
windatm <tuanphongmta0205@gmail.com>
2017-09-25 14:01:08 +0700

    FrmMain1

--- QLBanHang/QLBanHang/GUI/FrmMain.cs
+++ QLBanHang/QLBanHang/GUI/FrmMain.cs
@@ -13,6 +13,50 @@ namespace QLBanHang.GUI
 {
     public partial class FrmMain : Form
     {
-       
+        private QLBanHangDbContext db = Service.DBService.db;
+        private NHANVIEN nv = new NHANVIEN();
+
+        #region constructor
+        public FrmMain(NHANVIEN _nv)
+        {
+            InitializeComponent();
+            Service.DBService.Reload();
+            nv = _nv;
+        }
+        #endregion
+
+        #region LoadForm
+
+        private void LoadPhanQuyen()
+        {
+            if (nv.QUYEN == 1)
+            {
+                // phan quyen admin
+                btnQLNhanVien.Enabled = true;
+                btnQLMatHang.Enabled = true;
+                btnQLKho.Enabled = true;
+                btnXuatHang.Enabled = true;
+                btnNhapHang.Enabled = true;
+                return;
+            }
+
+            if (nv.QUYEN == 0)
+            {
+                // phan quyen nhan vien
+                btnQLNhanVien.Enabled = false;
+                btnQLMatHang.Enabled = false;
+                btnQLKho.Enabled = true;
+                btnXuatHang.Enabled = true;
+                btnNhapHang.Enabled = true;
+            }
+        }
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            LoadPhanQuyen();
+            txtTTNhanVien.Text = nv.TEN + " - " + ((nv.QUYEN == 0) ? "Nhân viên" : "Quản trị");
+        }
+        #endregion
+
+
     }
 }
