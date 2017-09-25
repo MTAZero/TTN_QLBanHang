d7198d033d199f27e0c1fcffc4a535e7ccb8fbcd
windatm <tuanphongmta0205@gmail.com>
2017-09-25 14:45:30 +0700

    FrmNhapHang1

--- QLBanHang/QLBanHang/GUI/FrmNhapHang.cs
+++ QLBanHang/QLBanHang/GUI/FrmNhapHang.cs
@@ -13,7 +13,78 @@ namespace QLBanHang.GUI
 {
     public partial class FrmNhapHang : Form
     {
-       
+        private QLBanHangDbContext db = Service.DBService.db;
+        private int indexPhieuNhap = 0, indexPhieuNhap1 = 0;
+        private int indexChiTietNhap = 0, indexChiTietNhap1 = 0;
+        private NHANVIEN nv = new NHANVIEN();
+
+        #region constructor
+        public FrmNhapHang(NHANVIEN _nv)
+        {
+            InitializeComponent();
+            Service.DBService.Reload();
+            nv = _nv;
+        }
+        #endregion
+
+        #region Form chính
+        private void FrmNhapHang_Load(object sender, EventArgs e)
+        {
+            LoadPhieuNhap();
+            LoadChiTietNhap();
+        }
+        #endregion
+
+        #region Phiếu nhập
+
+        #region Load
+        private void LoadInitControl()
+        {
+            // cbx Nhân viên
+            cbxNhanVien.DataSource = db.NHANVIENs.ToList();
+            cbxNhanVien.ValueMember = "ID";
+            cbxNhanVien.DisplayMember = "TEN";
+
+            dateNgayNhap.Value = DateTime.Now;
+
+            groupThongTinPhieuNhap.Enabled = false;
+
+        }
+
+        private void LoadDgvPhieuNhap()
+        {
+            int i = 0;
+            var dataPhieuNhap = db.PHIEUNHAPs.ToList()
+                                .Select(p => new
+                                {
+                                    ID = p.ID,
+                                    STT = ++i,
+                                    Ngay = ((DateTime)p.NGAY).ToString("dd/MM/yyyy"),
+                                    NhanVien = db.NHANVIENs.Where(z => z.ID == p.NHANVIENID).FirstOrDefault().TEN,
+                                    DiaDiem = p.DIADIEM,
+                                    TongTien = p.TONGTIEN
+                                })
+                                .ToList();
+            dgvPhieuNhap.DataSource = dataPhieuNhap;
+
+            // thêm index 
+            indexPhieuNhap = indexPhieuNhap1;
+            try
+            {
+                dgvPhieuNhap.Rows[indexPhieuNhap].Cells["STTPhieuNhap"].Selected = true;
+                dgvPhieuNhap.Select();
+            }
+            catch { }
+
+            LoadDgvChiTietNhap();
+        }
+
+        private void LoadPhieuNhap()
+        {
+            LoadInitControl();
+            LoadDgvPhieuNhap();
+        }
+        #endregion
 
     }
 }
