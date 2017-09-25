c726686594092e8da143dae8b8b7011f2794ab8e
taikhoana1 <noidovatoi11196@gmail.com>
2017-09-25 14:06:44 +0700

    form doi mk l1

--- QLBanHang/QLBanHang/GUI/FrmDoiMatKhau.cs
+++ QLBanHang/QLBanHang/GUI/FrmDoiMatKhau.cs
@@ -13,6 +13,43 @@ namespace QLBanHang.GUI
 {
     public partial class FrmDoiMatKhau : Form
     {
-        
+        private QLBanHangDbContext db = Service.DBService.db;
+        private NHANVIEN nv = new NHANVIEN();
+
+        #region Load form
+        public FrmDoiMatKhau(NHANVIEN _nv)
+        {
+            InitializeComponent();
+            Service.DBService.Reload();
+            nv = _nv;
+        }
+        #endregion
+
+        private void btnDoiMatKhau_Click(object sender, EventArgs e)
+        {
+            if (txtMatKhauCu.Text != nv.MATKHAU)
+            {
+                MessageBox.Show("Mật khẩu cũ không chính xác", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (txtMatKhauMoi.Text == "")
+            {
+                MessageBox.Show("Mật khẩu mới không được để trống", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (txtXacNhan.Text != txtMatKhauMoi.Text)
+            {
+                MessageBox.Show("Xác nhận mật khẩu không chính xác", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            nv.MATKHAU = txtMatKhauMoi.Text;
+            db.SaveChanges();
+
+            MessageBox.Show("Đổi mật khẩu thành công", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            this.Close();
+        }
     }
 }
